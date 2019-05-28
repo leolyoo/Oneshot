481d0f3509282a88ee156082aba6f68cd46e0218
leolyoo <leolyoo96@naver.com>
2019-05-28 14:39:36 +0900

    GameResultImage added

--- app/src/main/java/com/example/a82102/oneshot/GameResultActivity.java
+++ app/src/main/java/com/example/a82102/oneshot/GameResultActivity.java
@@ -4,7 +4,6 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.widget.ImageView;
 import android.widget.TextView;
 
